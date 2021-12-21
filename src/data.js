e0285cfad7cd1a2f04059a3ad0fa84afb9e98e61
DhruvMoradiya49 <dhruvmoradiya49@gmail.com>
2021-12-21 10:51:28 -0500

    image change

--- src/data.js
+++ src/data.js
@@ -47,8 +47,8 @@ export const featuredPortfolio = [
     },
     {
       id: 6,
-      title: "Travel Webpage",
-      img: "https://cdn.dribbble.com/users/6384483/screenshots/15468426/media/ce9479fa1f8dba3a4a49840d76e55e31.png?compress=1&resize=1200x900",
+      title: "B2B Dashboard",
+      img: process.env.PUBLIC_URL + "/assets/dashboard.png",
     },
   ];
   
